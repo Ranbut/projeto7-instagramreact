9e016a2f29dd04ca0f8288568a8ff43bd65234a6
Ranbut <Ranbut_Beater@protonmail.com>
2022-11-21 09:17:43 -0300

    Validation of user name and img

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,17 +1,52 @@
 import { useState } from 'react'
+import validator from 'validator';
 
 export default function User(){
   const [userText, setUserText] = useState("Catana");
   const [userImage, setUserImage] = useState("./assets/img/catanacomics.svg");
 
+  function changeName() {
+    let text;
+
+    while(!text){
+      text = prompt("Insira seu nome:");
+      if (text === null) {
+        return; //Botão de cancelar
+    }
+      if(!text){
+        alert("Não pode deixar o nome vazio!");
+      }
+  }
+  setUserText(text);
+  }
+
+  function changeImage() {
+    let text;
+
+    while(!text){
+      text = prompt("Insira a URL da sua imagem:");
+      if (text === null) {
+        return; //Botão de cancelar
+    }
+      if(!text){
+        alert("Não pode deixar a URL vazio!");
+      }
+      if(text && !validator.isURL(text)){
+        text = "";
+        alert("Texto não é URL!");
+      }
+  }
+  setUserImage(text);
+  }
+
   return(
         <div data-test="user" className="div-profile">
         <div>
-          <img data-test="profile-image" onClick={() => setUserImage(prompt("Insira URL da sua imagem:"))} src={userImage} alt="catanacomics"/>
+          <img data-test="profile-image" onClick={changeImage} src={userImage} alt="catanacomics"/>
         </div>
         <div>
           <p className="profile-username">catanacomics</p>
-          <p data-test="name" className="profile-name">{userText}<ion-icon data-test="edit-name" onClick={() => setUserText(prompt("Insira seu nome:"))} name="pencil-outline"></ion-icon></p>
+          <p data-test="name" className="profile-name">{userText}<ion-icon data-test="edit-name" onClick={changeName} name="pencil-outline"></ion-icon></p>
         </div>
       </div>
     );
